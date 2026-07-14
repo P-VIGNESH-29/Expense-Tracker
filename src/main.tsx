7a4564b91d88bcadff019ac9a1200edf5bea4e17
P Vignesh <p.vignesh2909@gmail.com>
2026-07-14 11:07:53 +0530

    Added new feature

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
 import '@mantine/core/styles.css';
@@ -12,12 +12,11 @@ import { SettingsProvider } from './context/SettingsContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-     <Provider store={store}>
-       <SettingsProvider>
-         <App />
-         <RouterProvider router={router}/>
-       </SettingsProvider>
-     </Provider>
+ <Provider store={store}>
+  <SettingsProvider>
+    <RouterProvider router={router} />
+  </SettingsProvider>
+</Provider>
   </StrictMode>,
 )
 
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+// import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Dashboard from "../pages/dashboard/Dashboard";
 import Login from "../pages/login/Login";
@@ -8,51 +8,56 @@ import Profile from "../pages/profile/Profile";
 import AddExpenses from "../pages/Addexpenses/AddExpenses";
 import ProfileSettings from "../pages/profile-settings/ProfileSettings";
 import EditMonthlyBill from "../pages/profile-settings/EditMonthlyBill";
+import { createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/Layout",
+      element: <Layout />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: "Dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "Expenses",
+          element: <Expenses />,
+        },
+        {
+          path: "Categories",
+          element: <Categories />,
+        },
+        {
+          path: "Profile",
+          element: <Profile />,
+        },
+        {
+          path: "ProfileSettings",
+          element: <ProfileSettings />,
+        },
+        {
+          path: "EditMonthlyBill/:id",
+          element: <EditMonthlyBill />,
+        },
+        {
+          path: "AddExpenses",
+          element: <AddExpenses />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/Layout",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: "Dashboard",
-        element: <Dashboard />,
-      },
-      {
-        path: "Expenses",
-        element: <Expenses />,
-      },
-      {
-        path: "Categories",
-        element: <Categories />,
-      },
-      {
-        path: "Profile",
-        element: <Profile />,
-      },
-      {
-        path: "ProfileSettings",
-        element: <ProfileSettings />,
-      },
-      {
-        path: "EditMonthlyBill/:id",
-        element: <EditMonthlyBill />,
-      },
-      {
-        path: "AddExpenses",
-        element: <AddExpenses />,
-      },
-    ],
-  },
-]);
+    basename: "/Expense-Tracker",
+  }
+);
 
 export default router;
-
